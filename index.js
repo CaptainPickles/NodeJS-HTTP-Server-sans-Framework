8bfe2f567470bb4b6a27331fddb20fbc596109b8
Lucas Levesque <levesque.lucas@outlook.fr>
2022-05-02 15:55:27 +0200

    feat : creating async func to read html

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const fs = require('fs');
 const http = require('http');
 
 const routes = ["/"]
 
-http.createServer(function (req, res) {
+async function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
+http.createServer(async function (req, res) {
     try {
         res.writeHead(200, { 'Content-Type': 'text/html' }); // http header
         const url = req.url;
@@ -13,7 +25,9 @@ http.createServer(function (req, res) {
             res.end();
         }
         else if (url === '/' && method === "GET") {
-            res.write('<h1>HELLO WORLD Lucas</h1>');
+
+            const content = await readFile(`${__dirname}/public/pages/index.html`)
+            res.write(content);
             res.end();
         } else if (url === '/' && method !== "GET") {
             res.statusCode = 403
@@ -22,6 +36,7 @@ http.createServer(function (req, res) {
         }
 
     } catch (error) {
+        console.log(error)
         res.statusCode = 500
         res.write(`<h1>${res.statusCode} Erreur Interne au Serveur</h1>`);
         res.end()
