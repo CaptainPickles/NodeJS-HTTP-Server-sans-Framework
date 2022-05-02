59eae67a3780a4468e693064ebd4efac6a476f23
Lucas Levesque <levesque.lucas@outlook.fr>
2022-05-02 15:23:32 +0200

    FEAT : Q3.1 done

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ http.createServer(function (req, res) {
     if (url === '/' && method === "GET") {
         res.write('<h1>HELLO WORLD Lucas</h1>');
         res.end();
+    } else if (url === '/' && method !== "GET") {
+        const statusCode = 403
+        res.statusCode = statusCode
+        res.write(`<h1>${statusCode} Méthode non authorisée</h1>`);
+        res.end()
     }
 
 }).listen(5000);
