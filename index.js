3e08854cf138c4045ef20e56afb1a4cf42e25d88
Lucas Levesque <levesque.lucas@outlook.fr>
2022-05-02 15:35:29 +0200

    refacto code statuscode

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ const http = require('http');
 const routes = ["/"]
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' }); // http header
-    const url = req.url;
-    const method = req.method
-    if (routes.includes(url) === false) {
-        res.statusCode = 404
-        res.write(`<h1>${res.statusCode} Page Introuvable</h1>`);
-        res.end();
-    }
-    else if (url === '/' && method === "GET") {
-        res.write('<h1>HELLO WORLD Lucas</h1>');
-        res.end();
-    } else if (url === '/' && method !== "GET") {
-        const statusCode = 403
-        res.statusCode = statusCode
+    try {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' }); // http header
+        const url = req.url;
+        const method = req.method
+        if (routes.includes(url) === false) {
+            res.statusCode = 404
+            res.write(`<h1>${res.statusCode} Page Introuvable</h1>`);
+            res.end();
+        }
+        else if (url === '/' && method === "GET") {
+            res.write('<h1>HELLO WORLD Lucas</h1>');
+            res.end();
+        } else if (url === '/' && method !== "GET") {
+            res.statusCode = 403
+            res.write(`<h1>${res.statusCode} Méthode non authorisée</h1>`);
+            res.end()
+        }
+
+    } catch (error) {
+        res.statusCode = 500
         res.write(`<h1>${res.statusCode} Méthode non authorisée</h1>`);
         res.end()
     }
